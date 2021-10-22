dc7b173f591498d62fa0dc05fa8c5faa98c2bf24
laoappsdev6 <laoappsdev6@gmail.com>
2021-10-22 11:50:08 +0700

    last update

--- server.ts
+++ server.ts
@@ -1,7 +1,14 @@
 import * as https from 'https';
 import * as fs from 'fs';
+import * as express from 'express';
+import * as http from 'http'
 
-const port = 45689;
+const app = express();
+const httpPort = 45688;
+const httpsPort = 45689;
+
+
+
 const options = {
     key: fs.readFileSync('key.pem'),
     cert: fs.readFileSync('cert.pem')
@@ -10,7 +17,16 @@ const options = {
 https.createServer(options, function (req, res) {
     console.log("message", req)
     res.writeHead(200);
-    res.end("hello world\n");
-}).listen(port, '0.0.0.0', () => {
-    console.log("HTTPS Server is running on port " + port);
+    res.end("HTTPS say hello world\n");
+}).listen(httpsPort, '0.0.0.0', () => {
+    console.log("HTTPS Server is running on port " + httpsPort);
 });
+
+
+app.post("/", (req, res) => {
+    res.send("hello world")
+})
+http.createServer(app)
+    .listen(httpPort, '0.0.0.0', () => {
+        console.log("My Server is running on port " + httpPort);
+    });
